4aaffab772061ff590b8cc549e4e21899e22844d
DaviCorreiaGasoso <davicorreiao07@gmail.com>
2024-04-26 14:54:36 -0300

    Testes de Label e Button

--- testelabel.py
+++ testelabel.py
@@ -1,9 +1,17 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.utils import get_color_from_hex
 
 class MinhaAplicacao(App):
     def build(self):
-        return Label(text='Olá SESI/SENAI', font_size = 100, font_name='Arial')
+        return Label(
+            text='Olá SESI',
+            halign='left',
+            valign='top',
+            size_hint = (None, None),
+            size = (150, 50),
+            text_size=(150,None)
+            )
 
 if __name__ == '__main__':
     MinhaAplicacao().run()
